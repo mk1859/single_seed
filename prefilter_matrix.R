256b836ae8eaee020bfaeccd1f01da78163bfad4
mk1859 <mkrzyszton.wtm@gmail.com>
2021-10-04 11:39:04 +0200

    Update prefilter_matrix.R

--- prefilter_matrix.R
+++ prefilter_matrix.R
@@ -2,20 +2,20 @@
 
 prefilter_matrix <- function(matrix, mean_exp, n_reads) {
   
-  filt_mat <- matrix [-c (grep ("__", rownames (matrix)),                                 #remove summary statistics
-                            grep ("ATC", rownames (matrix)),                              #remove chloroplast genes
-                            grep ("ATM", rownames (matrix))),]                            #remove mitochondrial genes
+  filt_mat <- matrix [-c (grep ("__", rownames (matrix)),                                 # remove summary statistics
+                            grep ("ATC", rownames (matrix)),                              # remove chloroplast genes
+                            grep ("ATM", rownames (matrix))),]                            # remove mitochondrial genes
   
   # remove genes non encoding proteins based on prepared earlier gene description file
   
   filt_mat <- filt_mat [which (rownames(filt_mat) %in% 
                                  Araport [Araport$type == "protein_coding","gene"]),]
                                  
-  #remove genes below set average expression threshold
+  # remove genes below set average expression threshold
   
   filt_mat <- filt_mat [rowMeans(filt_mat) > mean_exp,]
   
-  #remove seeds with less then set number of sequenced reads
+  # remove seeds with less then set number of sequenced reads
   
   filt_mat <- filt_mat [,colSums(filt_mat)> n_reads]
   
