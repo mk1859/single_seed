1b14ad9af5523d41ae2ff887195d0a42726be888
mk1859 <mkrzyszton.wtm@gmail.com>
2021-10-06 15:51:04 +0200

    Create pca_continuous.R

---
+++ R/pca_continuous.R
@@ -0,0 +1,25 @@
+# PCA plot with coloring of continuous values
+
+pca_continuous <- function(seurat_obj, column, excluded = NULL) {
+  require (Seurat)
+  require (ggplot2)
+  
+  # export data from Seurat object
+  plot <- cbind (as.data.frame (Embeddings(object = seurat_obj, reduction = "pca"))  [,1:2], seurat_obj@meta.data) 
+  
+  # exclude some time points if necessary
+  if (!is.null(excluded )){
+    plot <-  plot [-grep( excluded, rownames(plot)),]
+  }
+  
+  g <- ggplot(plot, aes(x=PC_1, y= PC_2, color = .data[[column]])) +
+            geom_point (size = 1) + 
+            scale_colour_viridis() +
+            theme_classic () +
+            theme(axis.line=element_blank(),
+                  axis.text=element_blank(),
+                  axis.ticks=element_blank(),
+                  axis.title=element_blank()) 
+  
+  return (g)
+}
