59cfabd7b1f80929bd1993a1e7c7fe3431e78898
mk1859 <mkrzyszton.wtm@gmail.com>
2021-10-06 21:59:57 +0200

    Update deg_list.R

--- R/deg_list.R
+++ R/deg_list.R
@@ -1,19 +1,25 @@
-# it wrapper function to Seurat FindMarkers that allows to compare gene expression pairwise between conditions named in two vectors
-# function generates list as out put which elements contain data frames with significant DEGs
+# wrapper function to Seurat FindMarkers that allows to pairwise compare gene expression between conditions named in two vectors
+# as input requires seurat obcject, column of metadata containing names of conditions, two vectors with condition names as well as 
+# padj and log2 fold change tresholds
+# function generates list as output which elements contain data frames with significant DEGs
 
-deg_list <- function(seurat_obj, vector1, vector2, padj = 0.05, log2FC_threshold = log2(2)) {
+deg_list <- function(seurat_obj, vector1, vector2, column, padj = 0.05, log2FC_threshold = log2(2)) {
   require (Seurat)
+  require (dplyr)
   
-  # set
-  seurat_obj@active.ident <- as.factor(seurat_obj$timepoint)
-  deg_list <- list ()
-  for (i in 1:length (vector1)){
-  deg_list [[i]] <- FindMarkers(object = seurat_obj, ident.1 = vector2 [i],
-                      ident.2 = vector1 [i],logfc.threshold = log2FC_threshold, 
+  # set column to active identity in Seurat object
+  seurat_obj@active.ident <- as.factor(seurat_obj$column)
+  
+  # DEG identification
+  deg_list <- mapply (function(vector1,vector2) {
+    FindMarkers(object = seurat_obj, ident.1 = vector2,
+                      ident.2 = vector1,logfc.threshold = log2FC_threshold, 
                       test.use = "wilcox",only.pos =FALSE,
                       assay = "SCT", slot ="data") %>%
-                    filter(.,p_val_adj < padj)
-  }
+    filter(.,p_val_adj < padj)
+    }, vector1, vector2)
+  
+  # set names to results
   names(deg_list) <- paste0(vector1, "_", vector2)
   
   return (deg_list)
