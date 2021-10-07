08fb176a8ffca65d2941c81a986aeb285c75ecd2
mk1859 <mkrzyszton.wtm@gmail.com>
2021-10-07 13:52:40 +0200

    Update deg_list.R

--- R/deg_list.R
+++ R/deg_list.R
@@ -4,23 +4,24 @@
 # function generates list as output which elements contain data frames with significant DEGs
 
 deg_list <- function(seurat_obj, vector1, vector2, column, padj = 0.05, log2FC_threshold = log2(2)) {
-  require (Seurat)
-  require (dplyr)
-  
-  # set column to active identity in Seurat object
-  seurat_obj@active.ident <- as.factor(seurat_obj$column)
-  
-  # DEG identification
-  deg_list <- mapply (function(vector1,vector2) {
-    FindMarkers(object = seurat_obj, ident.1 = vector2,
+    require (Seurat)
+    require (dplyr)
+    
+    # set column to active identity in Seurat object
+    seurat_obj@active.ident <- as.factor(seurat_obj [[column]] [,1])
+    names (seurat_obj@active.ident) <- rownames (seurat_obj@meta.data)
+    
+    # DEG identification
+    deg_list <- mapply (function (vector1,vector2) {
+          FindMarkers(object = seurat_obj, ident.1 = vector2,
                       ident.2 = vector1,logfc.threshold = log2FC_threshold, 
                       test.use = "wilcox",only.pos =FALSE,
                       assay = "SCT", slot ="data") %>%
-    filter(.,p_val_adj < padj)
-    }, vector1, vector2)
-  
-  # set names to results
-  names(deg_list) <- paste0(vector1, "_", vector2)
-  
-  return (deg_list)
+                    filter(.,p_val_adj < padj)},
+          vector1,vector2, SIMPLIFY = FALSE)
+    
+    # set names to results
+    names(deg_list) <- paste0(vector1, "_", vector2)
+    
+    return (deg_list)
 }
