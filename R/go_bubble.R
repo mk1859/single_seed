d55203122d6afe26e5315fc409aecc6ed9c9c1a3
mk1859 <mkrzyszton.wtm@gmail.com>
2021-10-08 15:30:35 +0200

    Update go_bubble.R

--- R/go_bubble.R
+++ R/go_bubble.R
@@ -2,7 +2,8 @@
 
 go_bubble <- function(res_file_single) {
    require (ggplot2)
-       
+   require (ggthemes)
+  
   # add fold change enrichemnet of the GO terms
   res_file_single$FC <- (res_file_single$intersection_size / res_file_single$query_size) /
                         (res_file_single$term_size / res_file_single$effective_domain_size)
