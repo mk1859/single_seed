6ea088d2a57492166fb95aeb7504d8180eda3675
mk1859 <mkrzyszton.wtm@gmail.com>
2021-10-09 13:05:52 +0200

    Update list_seurat.R

--- R/list_seurat.R
+++ R/list_seurat.R
@@ -1,31 +1,31 @@
 # function for creating Seurat objects for list of treatments 
 # with batch removal and background regression duing sctransform normalization
 
-list_seurat <- function (selected_genes, background) {
+list_seurat <- function (matrix_list, background) {
   require (Seurat)
   require (sctransform)
 
-  # list of seed attributes, take treatment and batch information from seed name
-  timepoint_attr <- lapply (selected_genes, function (x) { 
+  # list of seed attributes, take batch information from seed name
+  timepoint_attr <- lapply (matrix_list, function (x) { 
                             data.frame(batch = gsub('.{0,3}$', '', colnames(x)),
                             background = background [names(background) %in% colnames(x)] ) })
                             
-  # creating Seurat objects
+  # create list of Seurat objects
   timepoint_seurats <- mapply (function (x,y) {
-          CreateSeuratObject(counts = x, meta.data = y)},
-          matrix_list,timepoint_attr)
-  # normalizing Seurat objects
+                          CreateSeuratObject(counts = x, meta.data = y)},
+                       matrix_list,timepoint_attr)
+  
+  # normalize Seurat objects
   timepoint_seurats <- lapply (timepoint_seurats, function (x) {
                           Seurat::SCTransform(object = x, verbose =FALSE, 
                                               n_genes = NULL, 
                                               batch_var = "batch",
-                                                method = "nb", 
+                                              method = "nb", 
                                               vars.to.regress ='background',
                                               return.only.var.genes = TRUE)})
-  # running PCA
+  # run PCA
   timepoint_seurats <- lapply (timepoint_seurats, function (x) {
-  RunPCA(object = x, verbose = FALSE)})
+                          RunPCA(object = x, verbose = FALSE)})
   
-  return (timepoint_seurats)
-  
+  return (timepoint_seurats) 
 }
