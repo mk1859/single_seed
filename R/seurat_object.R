1e30da84616f1e52edda0c1bd6f4c5cfa30a58f3
mk1859 <mkrzyszton.wtm@gmail.com>
2021-10-11 17:33:54 +0200

    Update seurat_object.R

--- R/seurat_object.R
+++ R/seurat_object.R
@@ -4,17 +4,17 @@ seurat_object <- function(matrix, background = NULL, include_background=TRUE) {
   require (Seurat)
   require (sctransform)
   
-  # meta data for each seed
+  # metadata for each seed
   seed_attr <- data.frame(n_reads = colSums(matrix),                          # number of reads
                   n_gene = colSums(matrix > 0),                               # number of genes
                   log10_reads = log10(colSums(matrix)),                       # log10 number of reads
-                  timepoint = gsub('.{0,6}$', '', colnames(matrix)),          # treatment
-                  batch = gsub('.{0,3}$', '', colnames(matrix)),              # batch
+                  timepoint = gsub('.{0,6}$', '', colnames(matrix)),          # treatment extracted from seed name
+                  batch = gsub('.{0,3}$', '', colnames(matrix)),              # batch extracted from seed name
                   background = background)                                    # fraction of background reads
                   
   seurat_seeds <- CreateSeuratObject(counts = matrix, meta.data = seed_attr)
   
-  # sctransform with optionall background fraction as value to regress
+  # sctransform with optional background reads fraction as value to regress
   if (include_background==TRUE) {
     seurat_seeds <- SCTransform(object = seurat_seeds, verbose =FALSE, n_genes = NULL,
                                 vars.to.regress = 'background', return.only.var.genes = TRUE)
