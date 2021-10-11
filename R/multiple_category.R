15eb052e61b80097c2bd600aa221cd1c24067fdb
mk1859 <mkrzyszton.wtm@gmail.com>
2021-10-11 17:05:44 +0200

    Update multiple_category.R

--- R/multiple_category.R
+++ R/multiple_category.R
@@ -1,4 +1,4 @@
-# analysis of multiple GO categories by warpping go_res.R function on BP, CC and MF ontology categories
+# analysis of multiple GO categories by wrapping go_res function on BP, CC and MF ontology categories
 
 multiple_category <- function(genes, background, p_value = 0.05, rrvgo_threshold=0.8) {
     require (gprofiler2)
@@ -6,7 +6,7 @@ multiple_category <- function(genes, background, p_value = 0.05, rrvgo_threshold
     require (org.At.tair.db)
     require (rlist)
     
-    # create list with ontologies
+    # create a list with ontologies
     sum_res <- list (BP = NULL, CC = NULL, MF = NULL)
     
     # use go_res for each ontology
@@ -14,7 +14,7 @@ multiple_category <- function(genes, background, p_value = 0.05, rrvgo_threshold
     sum_res$CC <- go_res (genes = genes, background = background, p_value = p_value, category = "CC", rrvgo_threshold=rrvgo_threshold)
     sum_res$MF <- go_res (genes = genes, background = background, p_value = p_value, category = "MF", rrvgo_threshold=rrvgo_threshold)
     
-    # combine to single data frame
+    # combine to a single data frame
     sum_res <- list.rbind(sum_res)
     
     # identify and remove ontologies without terms
