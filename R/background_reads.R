63ac2d68546171106d0c8a3882f63c68e27914bc
mk1859 <mkrzyszton.wtm@gmail.com>
2021-10-11 15:10:55 +0200

    Update background_reads.R

--- R/background_reads.R
+++ R/background_reads.R
@@ -1,9 +1,9 @@
-# function that requires both raw and pre-filtered matrices as input and returns fraction of intergenic reads per seed from pre-filtered matrix
+# function that uses both raw and pre-filtered matrices and returns the fraction of intergenic reads per seed from the pre-filtered matrix
 
 background_reads <- function(matrix, prefiltered) {
   require (dplyr)
   
-  #count background reads 
+  # count background reads 
   background_reads <- matrix  %>% 
   filter(rownames(matrix) %in% 
            c(Araport$gene 
@@ -11,12 +11,12 @@ background_reads <- function(matrix, prefiltered) {
                     grep ("ATC", rownames (matrix)),                       # chloroplast
                     grep ("ATM", rownames (matrix)))],                     # mitochondrial
                     "__no_feature"))  %>%                                  # intergenic
-    colSums()
+  colSums()
   
   # calculate fraction of intergenic reads
   background_reads <- background_reads/ colSums(matrix)
   
-  # filter for seeds remainig after prefiltering
+  # filter for seeds remaining after pre-filtering
   background_reads <- background_reads [names (background_reads)%in% colnames(prefiltered)]
                            
   return (background_reads)
