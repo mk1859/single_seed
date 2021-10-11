c5d2dd4f0b8154abe1a496febeb8bdced9b2bda5
mk1859 <mkrzyszton.wtm@gmail.com>
2021-10-11 16:56:01 +0200

    Update hvg.R

--- R/hvg.R
+++ R/hvg.R
@@ -1,13 +1,13 @@
-# function for identification of most varaible genes in sourat object, with number of top genes to set
+# function for identification of the most variable genes in Seurat object, with the top genes threshold to set
 
 hvg <- function(seurat_obj, top =500) {
     require (Seurat)
     require (matrixStats)
     
-    # export gnormalized gene ezpression form Seurat object
+    # export normalized gene expression from Seurat object
     norm <- GetAssayData(object = seurat_obj, assay = "SCT", slot = "data")
     
-    # callculate variance, crate data frame and order it
+    # calculate variance, create data frame and order it
     hvg <- rowVars (as.matrix(norm))
     hvg <- data.frame (gene = rownames(norm), var = hvg)
     hvg <- hvg [order (hvg$var, decreasing = TRUE),] [1:top,]
