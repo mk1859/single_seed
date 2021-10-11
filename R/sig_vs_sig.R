2f0725e8f5e3edd6e75c2d8daed4a97af2ed1aef
mk1859 <mkrzyszton.wtm@gmail.com>
2021-10-11 17:38:15 +0200

    Update sig_vs_sig.R

--- R/sig_vs_sig.R
+++ R/sig_vs_sig.R
@@ -1,18 +1,17 @@
 # signature vs signature plot
-# it is possible to exclude some treatments and requires adding order of treatments on the legend
-# coloumn of treatment to color the point need to be specify
+# it requires specifying column with treatment names, their order, it is possible to exclude one of the treatments
 
 sig_vs_sig <- function (seurat_obj, signature1, signature2, excluded = NULL, order, column) {
   require (Seurat)
   require (ggplot2)
   
-  # export data from Seurat object
+  # export data from the Seurat object
   plot <- seurat_obj@meta.data
   
   # set order of treatments
   plot$timepoint <- factor(plot$timepoint, levels = order)
   
-  # exclude some time points if necessary
+  # exclude treatment if necessary
   if (!is.null(excluded)){
     plot <-  plot [-grep( excluded, rownames(plot)),]
   }
